02fad2517e251c3016745b36eb4eb3a9baca466b
Gobinder2901 <dhillongobinder@gmail.com>
2026-05-15 20:05:50 -0600

    fixed docker location

--- qrcafe_staff/obj/Debug/net8.0/qrcafe_staff.AssemblyInfo.cs
+++ qrcafe_staff/obj/Debug/net8.0/qrcafe_staff.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("qrcafe_staff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f930b42f20c8eb43d49bfb695022aac43c4ff86")]
 [assembly: System.Reflection.AssemblyProductAttribute("qrcafe_staff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("qrcafe_staff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
